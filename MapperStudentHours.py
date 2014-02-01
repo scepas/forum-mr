a2e785dcbdf5f886e31950ff9534dcbeb12297da
scepas <scepas@gmail.com>
2014-02-01 14:50:44 +0100

    tabs to spaces

--- MapperStudentHours.py
+++ MapperStudentHours.py
@@ -6,20 +6,20 @@ from datetime import datetime
 
 # Expected input; tab separated file with enclosing double quotes.
 # 19 fields:
-#"id"	"title"	"tagnames"	"author_id"	"body"	"node_type"	"parent_id"	"abs_parent_id"	"added_at"	"score"	
-#"state_string"	"last_edited_id"	"last_activity_by_id"	"last_activity_at"	"active_revision_id"	"extra"	
-#"extra_ref_id"	"extra_count"	"marked"
+#"id"   "title" "tagnames"  "author_id" "body"  "node_type" "parent_id" "abs_parent_id" "added_at"  "score" 
+#"state_string" "last_edited_id"    "last_activity_by_id"   "last_activity_at"  "active_revision_id"    "extra" 
+#"extra_ref_id" "extra_count"   "marked"
 
 
 reader = csv.reader(sys.stdin, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n\r')
 
 for line in reader:
-	#make sure none of the expected fields is missing
-	if (len(line) != 19):
-		continue
+    #make sure none of the expected fields is missing
+    if (len(line) != 19):
+        continue
 
-	author_id = line[3]
-	#the date has the format 2012-02-25 08:09:06.787181+00
-	added_at = line[8][11:13]
-	
-	print("{0}\t{1}".format(author_id, added_at))
+    author_id = line[3]
+    #the date has the format 2012-02-25 08:09:06.787181+00
+    added_at = line[8][11:13]
+    
+    print("{0}\t{1}".format(author_id, added_at))
--- ReducerStudentHour.py
+++ ReducerStudentHour.py
@@ -9,23 +9,23 @@ hours = []
 #return the top 1 most common hour
 #if there are hours with equal count, an arbitray one is returned
 def get_most_frequent_hour(h):
-	return Counter(h).most_common(1)[0][0]
+    return Counter(h).most_common(1)[0][0]
 
 for line in sys.stdin:
-	data_mapped = line.strip().split("\t")
-	if (len(data_mapped) != 2):
-		continue
+    data_mapped = line.strip().split("\t")
+    if (len(data_mapped) != 2):
+        continue
 
-	this_student, this_hour = data_mapped
+    this_student, this_hour = data_mapped
 
-	if current_student and current_student != this_student:
-		h = get_most_frequent_hour(hours)
-		print("{0}\t{1}".format(current_student, h))
-		hours = []
-	
-	current_student = this_student
-	hours.append(this_hour)
-	
+    if current_student and current_student != this_student:
+        h = get_most_frequent_hour(hours)
+        print("{0}\t{1}".format(current_student, h))
+        hours = []
+    
+    current_student = this_student
+    hours.append(this_hour)
+    
 if current_student:
-	h = get_most_frequent_hour(hours)
-	print("{0}\t{1}".format(current_student, h))
+    h = get_most_frequent_hour(hours)
+    print("{0}\t{1}".format(current_student, h))
