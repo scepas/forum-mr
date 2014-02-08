71561cb56007009cdde6c04f2186d8289835bc10
scepas <scepas@gmail.com>
2014-02-08 12:13:18 +0100

    nodes are now unique

--- ReducerInvertedIndex.py
+++ ReducerInvertedIndex.py
@@ -2,24 +2,30 @@
 
 import sys
 
-
+current_line = None
 current_word = None
 nodes = []
+
 
 for line in sys.stdin:
 	data_mapped = line.strip().split("\t")
 	if (len(data_mapped) != 2):
 		continue
 
+	#if we've already processed this word-node combination, skip it
+	if current_line and current_line == data_mapped:
+		continue
+
 	this_word, this_node_id = data_mapped
 
 	if current_word and current_word != this_word:
-		print("{0}\t{1}".format(current_word, sorted(nodes)))
+		print("{0}\t{1}".format(current_word, sorted(set(nodes))))
 		current_word = this_word;
 		nodes = []
 
+	current_line = data_mapped
 	current_word = this_word
 	nodes.append(int(this_node_id))
 
 if current_word:
-	print("{0}\t{1}".format(current_word, sorted(nodes)))
+	print("{0}\t{1}".format(current_word, sorted(set(nodes))))
